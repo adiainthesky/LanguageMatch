b096c4bf596cdebd6193ff51ecd7bc3cdcfaf685
Adelaide Nalley <adiainthesky@gmail.com>
2020-09-21 17:29:16 -0700

    add initial script; add photos to results page

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,30 @@ $(document).ready(function() {
 
     if (end === "Front") {
       language = "Javascript";
+      $("#Python, #Ruby").hide();
+      $("#Javascript").show();
     } else if (ways === "manyWays") {
       language = "Ruby";
+      $("#Javascript, #Python").hide();
+      $("#Ruby").show();
     } else if (end === "Back" && ways ==="oneWay") {
       language = "Python";
+      $("#Javascript, #Ruby").hide();
+      $("#Python").show();
     } else if (speed === "Fast" && end != "Back") {
       language = "Javascript";
+      $("#Python, #Ruby").hide();
+      $("#Javascript").show();
     } else {
       language = "Python";
+      $("#Javascript, #Ruby").hide();
+      $("#Python").show();
     }
 
     $("#language").text(language);
     $("#match").show(); 
+    $("#languageChoice").hide();
+    $("h6").hide();
     
     event.preventDefault();
   });
